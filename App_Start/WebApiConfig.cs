04df70b1aade0ebb0991997decde72487ae16e04
DESKTOP-4F8B9BO\dell <ali-hasnain@alberunitech.com>
2021-06-30 18:12:05 +0500

    Completed

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace BackendCase
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
 
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BackendCase.Model;
 
 namespace BackendCase.Controllers
 {
     public class HomeController : Controller
     {
+        BackendConnectionString db  = new BackendConnectionString();
+
+
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
         }
+       
     }
 }
